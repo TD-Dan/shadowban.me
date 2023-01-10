02af59375c3232de218415acfa7098f319886707
Daniel Herkert <daniel.herkert@gmail.com>
2023-01-10 22:37:28 +0200

    Several improvements to usability and user feedback
    - Added selectable delay and expiration time.
    - Added exception handling
    - Added better file dumps on program abnormal termination (ctrl-c can be used safe now!)

--- tools/airdrop-wallet/airdrop-wallet.py
+++ tools/airdrop-wallet/airdrop-wallet.py
@@ -2,6 +2,7 @@ import os
 import time
 import argparse
 import json
+import pprint
 
 from iota_wallet import IotaWallet, StrongholdSecretManager
 from dotenv import load_dotenv
@@ -10,29 +11,15 @@ from dotenv import load_dotenv
 def main():
     ## Get commandline arguments
     parser = argparse.ArgumentParser(prog='airdrop-wallet', description='shadowban.me airdrop tool')
-
-    # # Required positional argument
     parser.add_argument('tool', type=str,
                         help='Tool to use: wallet-create, wallet-status, wallet-claim-all\n wallet-airdrop \naddresses-get')
-    # # Optional positional argument
-    # parser.add_argument('opt_pos_arg', type=int, nargs='?',
-    #                     help='An optional integer positional argument')
-    # Optional argument
     parser.add_argument('--input', type=str,
                         help='Filename of input file')
-                        
     parser.add_argument('--out', type=str,
                         help='Filename of output file')
-                        
     parser.add_argument('--amount', type=str,
                         help='Amount of smr/tokens')
-    # # Switch
-    # parser.add_argument('--switch', action='store_true',
-    #                     help='A boolean switch')
-
     args = parser.parse_args()
-
-    print("Using tool: " + args.tool)
 
 
     ## Load the .env variables
@@ -41,8 +28,12 @@ def main():
     # Get the stronghold password
     STRONGHOLD_PASSWORD = os.getenv('STRONGHOLD_PASSWORD')
     TOKEN_ID = os.getenv('TOKEN_ID')
+    DELAY_SECONDS = float(os.getenv('DELAY_SECONDS'))
+    EXPIRATION_SECONDS = int(os.getenv('EXPIRATION_SECONDS'))
 
     ## Setup wallet
+
+    print("Opening wallet...")
 
     wallet_options = {
         'nodes': ['https://api.shimmer.network'],
@@ -55,6 +46,8 @@ def main():
 
     wallet = IotaWallet('./_airdrop-database', wallet_options, coin_type, secret_manager)
 
+
+    print("Using tool: " + args.tool)
 
     match args.tool:
         case "addresses-get":
@@ -98,13 +91,16 @@ def main():
             account = wallet.get_account('Airdrop')
             
             response = account.sync_account()
-            print(f'Synced: {response}\n')
+            print("Synced:")
+            pprint.pprint(response)
 
-            balance = account.get_balance()
-            print(f'Balance: {balance}\n')
+            #balance = account.get_balance()
+            #print("Balance:")            
+            #pprint.pprint(balance)
             
             wallet_addr = account.addresses()
-            print(f'Address: {wallet_addr}\n')
+            print("Address:")
+            pprint.pprint(wallet_addr)
 
             output_ids = account.get_outputs_with_additional_unlock_conditions('All')
             print(f'Available outputs to claim: {output_ids}')
@@ -120,82 +116,106 @@ def main():
             print(f'Sent transaction: {transaction_result}')
 
         case "wallet-airdrop":
-            send_smr = False
-            in_file = args.input
-            amount = args.amount
-            if amount.startswith("SMR"):
-                send_smr = True
-                amount = amount[3:]
+            
             addresses = []
             failed_addresses = []
             success_addresses = []
             other_addresses = []
 
-            if in_file == None:
-                print("no input file specied: trying default 'addresses.txt'")
-                in_file = "addresses.txt"
+            send_smr = False
+            in_file = args.input
+            amount = args.amount
 
-            with open(in_file, encoding='utf8') as f:
-                if in_file.endswith(".txt"):
-                    addresses = f.readlines()
+            try:
+
+                if not amount:
+                    print("missing argument: --amount (amount of tokens as 123 or SMR123 for glow)")
+                    return
+                if amount.startswith("SMR"):
+                    send_smr = True
+                    amount = amount[3:]
+
+                if in_file == None:
+                    print("no input file specied: trying default 'addresses.txt'")
+                    in_file = "addresses.txt"
+
+                with open(in_file, encoding='utf8') as f:
+                    if in_file.endswith(".txt"):
+                        addresses = [line.rstrip() for line in f]
+                
+                print(f'Found {len(addresses)} addresses from {in_file}')
+
+
+                account = wallet.get_account('Airdrop')
+                
+                transaction_result = None
+                count = 1
+                total = len(addresses)
+
+
+                for addr in addresses:
+                    # sleep some to give time for client to process sync and previous transaction
+                    time.sleep(DELAY_SECONDS)
+
+                    #refresh account
+                    response = account.sync_account()
+
+                    if send_smr:
+                        print(f'\n[{count}/{total}] Sending {amount} glow (SMR) to {addr}')
+                        outputs = [{
+                            "address": addr,
+                            "amount": amount,
+                            "expiration": EXPIRATION_SECONDS
+                        }]
+                        transaction_result = account.send_amount(outputs)
+                    else:
+                        print(f'\n[{count}/{total}] Sending {amount} TOKENS to {addr}')
+                        outputs = [{
+                            "address": addr,
+                            "expiration": EXPIRATION_SECONDS,
+                            "nativeTokens": [(
+                                    TOKEN_ID,
+                                    hex(int(amount))
+                            )],
+                        }]
+                        transaction_result = account.send_native_tokens(outputs)
+
+
+                    if 'payload' in transaction_result:
+                        print(f'SENT transaction: {transaction_result["transactionId"]}')
+                        success_addresses.append(addr)
+                    elif 'error' in transaction_result:
+                        print(f'ERROR: {transaction_result}')
+                        failed_addresses.append(addr)
+                    else:
+                        print(f'OTHER: {transaction_result}')
+                        other_addresses.append(addr)
+                    
+                    count += 1
             
-            print(f'Found {len(addresses)} addresses from {in_file}')
-
-            account = wallet.get_account('Airdrop')
+            except FileNotFoundError as e:
+                print(f'\nInput file not found: {e.strerror} (use --input filename.txt to specify your own file)')
+            except Exception as e:
+                print("\n!!! Stopped because of:\n")
+                pprint.pprint(e)
+                raise
             
-            transaction_result = None
-
-            count = 1
-            total = len(addresses)
-
-            for addr in addresses:
-                addr = addr.strip()
-
-                #refresh account
-                response = account.sync_account()
-
-                if send_smr:
-                    print(f'\n[{count}/{total}] Sending {amount} glow (SMR) to {addr}')
-                    outputs = [{
-                        "address": addr,
-                        "amount": amount
-                    }]
-                    transaction_result = account.send_amount(outputs)
-                else:
-                    print(f'\n[{count}/{total}] Sending {amount} TOKENS to {addr}')
-                    outputs = [{
-                        "address": addr,
-                        "nativeTokens": [(
-                                TOKEN_ID,
-                                hex(int(amount))
-                        )],
-                    }]
-                    transaction_result = account.send_native_tokens(outputs)
-
-                if 'error' in transaction_result:
-                    print(f'ERROR: {transaction_result}')
-                    failed_addresses.append(addr)
-                elif 'payload' in transaction_result:
-                    print(f'SENT transaction: {transaction_result["transactionId"]}')
-                    success_addresses.append(addr)
-                else:
-                    print(f'OTHER: {transaction_result}')
-                    other_addresses.append(addr)
-                count += 1
-                time.sleep(3)
-            
-            if len(failed_addresses)>0:
-                with open("adw-failed.txt", mode="w", encoding="utf8") as f:
-                    for line in failed_addresses:
-                        f.write(line+'\n')
-            if len(success_addresses)>0:
-                with open("adw-success.txt", mode="w", encoding="utf8") as f:
-                    for line in success_addresses:
-                        f.write(line+'\n')
-            if len(other_addresses)>0:
-                with open("adw-other.txt", mode="w", encoding="utf8") as f:
-                    for line in other_addresses:
-                        f.write(line+'\n')
+            finally:
+                
+                total_failed = [addr for addr in addresses if addr not in success_addresses ]
+                print(f'\n\nSUCCESS: {len(success_addresses)}, FAIL: {len(failed_addresses)}, FAIL BY NO TRY: {len(total_failed)-len(failed_addresses)}, OTHER: {len(other_addresses)}\n')
+                if len(success_addresses)>0:
+                    with open("result-success.txt", mode="w", encoding="utf8") as f:
+                        for line in success_addresses:
+                            f.write(line+'\n')
+                if len(total_failed)>0:
+                    with open("result-failed.txt", mode="w", encoding="utf8") as f:
+                        for line in total_failed:
+                            f.write(line+'\n')
+                if len(other_addresses)>0:
+                    with open("result-other.txt", mode="w", encoding="utf8") as f:
+                        for line in other_addresses:
+                            f.write(line+'\n')
 
         case _:
             print(args.tool + " is not a tool")
